26ffe8307987dacdcd1f795fb12f933ba6e0a324
Alix Philibert <alix2035@gmail.com>
2023-06-15 00:22:00 +0200

    more info button working

--- frontend/src/components/objectmain/object/Object.jsx
+++ frontend/src/components/objectmain/object/Object.jsx
@@ -1,5 +1,13 @@
 /* eslint-disable react/destructuring-assignment */
+import { useState } from "react";
+
 function Objects(obj) {
+  const [clicked, setClicked] = useState(false);
+
+  const handlebutton = () => {
+    setClicked(!clicked);
+  };
+
   return (
     <div>
       <h6 className="oui">
@@ -7,7 +15,10 @@ function Objects(obj) {
         {obj.object.name}
         {obj.object.type}
         {obj.object.value}
-        {obj.object.info}
+        <button type="button" className="button-info" onClick={handlebutton}>
+          {clicked === true ? "moins d'info" : "plus d'info"}
+        </button>
+        {clicked === true ? obj.object.info : null}
       </h6>
     </div>
   );
